e25f634b946e89d76b888095a1ea3a06d3b51ffa
Tomas <voznickatomas@gmail.com>
2016-11-01 00:25:21 +0100

    uprava přidaní sportoviste a kurtu

---
+++ RPRO_SportSoft/Controllers/CourtsController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPRO_SportSoft.Controllers
+{
+    public class CourtsController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        int s;
+        // GET: Courts
+        public ActionResult Index()
+        {
+            return View(db.Courts.ToList());
+        }
+
+        // GET: Courts/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Courts/Create
+        public ActionResult Create(int sport)
+        {
+           this.s = sport;
+            return View();
+        }
+
+        // POST: Courts/Create
+        [HttpPost]
+        public ActionResult Create(String CourtName)
+        {
+            try
+            {
+                Court k = new Court();
+                k.Name = CourtName;
+                k.Sports_Id = s;
+                db.Courts.InsertOnSubmit(k);
+                db.SubmitChanges();
+               
+                return RedirectToAction("Details", "Sports" ,new { id = s });
+            }
+            catch(System.Data.SqlClient.SqlException e )
+            {
+                e.ToString();
+                return View();
+            }
+        }
+
+        // GET: Courts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Courts/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Courts/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(db.Courts.Where(Court => Court.Id == id).Single());
+        }
+
+        // POST: Courts/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var item = db.Courts.Where(Court => Court.Id == id).Single();
+                db.Courts.DeleteOnSubmit(item);
+                db.SubmitChanges();
+
+                return RedirectToAction("Index","Main");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RPRO_SportSoft/Controllers/SportsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPRO_SportSoft.Controllers
+{
+    public class SportsController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        // GET: Sports
+        public ActionResult Index()
+        {
+            return View(db.Sports.ToList());
+            
+        }
+        // GET: Sports/Details/5
+        public ActionResult Details(int? id)
+        {
+            var item = db.Courts.Where(Court => Court.Sports_Id == id);
+
+            return View(item.ToList());
+        }
+        // GET: Sports/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sports/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Id,Name")] Sport s)
+        {
+            try
+            {
+                db.Sports.InsertOnSubmit(s);
+                db.SubmitChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sports/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(db.Sports.Where(Sport => Sport.Id == id).Single());
+        }
+
+        // POST: Sports/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var item = db.Sports.Where(Sport => Sport.Id == id).Single();
+               db.Sports.DeleteOnSubmit(item);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
