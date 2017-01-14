b67974fb97988aa3f1bc9254e2445396d9af0166
Tomas <voznickatomas@gmail.com>
2017-01-14 19:29:06 +0100

    Zavedení zakladu pro testovaní + test add sport

---
+++ RPRO_SportSoftTests/Application/CourtsAppTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPRO_SportSoft.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPRO_SportSoft.Application.Tests
+{
+    [TestClass()]
+    public class CourtsAppTests
+    {
+        [TestMethod()]
+        public void CheckForWhiteSpacesTest()
+        {
+          /*  CourtsApp app = new CourtsApp();
+            var ress = app.CheckForWhiteSpaces(" test ");
+            var exp = "test";
+            Assert.AreEqual(exp, ress);*/
+
+        }
+    }
+}
---
+++ RPRO_SportSoftTests/Application/SportsAppTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPRO_SportSoft.Application;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPRO_SportSoft.Application.Tests
+{
+    [TestClass()]
+    public class SportsAppTests
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext(System.Configuration.ConfigurationManager.ConnectionStrings["TestDatabase"].ConnectionString);
+            
+        [TestMethod()]
+        public void AddTest()
+        {
+            SportsApp app = new SportsApp("TestDatabase");
+
+            var ress = app.Add("Test", "No_photo.jpg");
+            var exp = true;
+
+            var item = db.Sports.Where(Sport => Sport.Name == "Test").Single();
+            db.Sports.DeleteOnSubmit(item);
+            db.SubmitChanges();
+
+            Assert.AreEqual(exp, ress);
+                      
+        }
+    }
+}
